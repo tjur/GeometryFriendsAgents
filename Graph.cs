d3053b9f835a945d5dfccaa7c35b5ce760ae40fc
Tjur <tommy.jurkiewicz@gmail.com>
2017-02-05 15:16:30 +0100

    Create vertex types; create vertices in start points
    - Added Obstacle and Type fields in Vertex class
    - Created Graph class and moved Vertex definition to it

---
+++ Graph.cs
@@ -0,0 +1,52 @@
+﻿using GeometryFriends.AI.Perceptions.Information;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeometryFriendsAgents
+{
+    class Graph
+    {
+        public List<Vertex> Vertices { get; }
+
+        public Graph()
+        {
+            Vertices = new List<Vertex>();
+        }
+
+        // dla danej przeszkody zwraca wszystkie wierzchołki, które się na niej znajdują (posortowane rosnąco po X)
+        public List<Vertex> GetAllVerticesOnObstacle(ObstacleRepresentation? obstacle)
+        {
+            return Vertices.Where(vertex => vertex.Obstacle.Equals(obstacle)).OrderBy(vertex => vertex.X).ToList();
+        }
+    }
+
+    // typy wierzchołków
+    enum VertexType { OnCircleStart, OnRectangleStart, OnObstacleLeft, OnObstacleRight, OnWholeObstacle, OnCollectible, UnderCollectible };
+
+    // wierzchołek grafu, będący tak naprawdę prostokątem na planszy
+    class Vertex
+    {
+        // przechowywana pozycja to pozycja środka
+        public float X { get; }
+        public float Y { get; }
+        public float Width { get; }
+        public float Height { get; }
+
+        // przeszkoda na której znajduje się wierzchołek lub null (dla typu OnCollectible)
+        public ObstacleRepresentation? Obstacle { get; }
+
+        public VertexType Type { get; }
+
+        public Vertex(float X, float Y, float Width, float Height, VertexType Type, ObstacleRepresentation? Obstacle = null)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Width = Width;
+            this.Height = Height;
+            this.Type = Type;
+            this.Obstacle = Obstacle;
+        }
+    }
+}
